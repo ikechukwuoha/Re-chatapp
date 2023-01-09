884dd17ee5924b9363d60e6e723a2688b5f68b92
Ikechukwu <ikechukwuandrew4@gmail.com>
2023-01-09 14:06:19 +0100

    Adding email settings to settings.py

--- corepro/settings.py
+++ corepro/settings.py
@@ -135,3 +135,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'users.MyUser'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_FROM = 'mohanugodu@gmail.com'
+EMAIL_HOST_USER = 'mohanugodu@gmail.com'
+EMAIL_HOST_PASSWORD = 'icligpklhxmgayjh'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+PASSWORD_RESET_TIMEOUT = 14400
